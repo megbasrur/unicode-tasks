d43acf39f79051e665405864710ad1c40c4dbb02
megbasrur <megbasrur@gmail.com>
2024-08-05 22:39:43 +0530

    Delete components directory

--- components/Button.js
+++
@@ -1,9 +0,0 @@
-import "./Button.css";
-
-
-export const Button=({symbol, color, handleClick})=>{
-    return (
-        <div onClick={()=>handleClick(symbol)} className="button-container" style={{backgroundColor: color}}>{symbol}</div>
-    )
-}
-
